189d0a9ce2659dfb77c2cd8f1d1a063fb10606fc
Niklas Wildenburg <niklascanisius@me.com>
2026-03-18 15:17:39 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs @ {
+    self,
+    nix-darwin,
+    nixpkgs,
+  }: let
+    configuration = {pkgs, ...}: {
+      environment = {
+        shellAliases = {
+          clean-brew-list = "brew cleanup; clear; brew list";
+          garbage = "docker system prune --all -f";
+          lisha = "ls -lisha";
+          nix-edit = "code /etc/nix-darwin";
+          nix-rebuild = "clear; alejandra /etc/nix-darwin; sudo darwin-rebuild switch --flake /etc/nix-darwin";
+          ssh-all = "~/code/os/assets/ssh-all.sh";
+          ssh-list = "~/code/os/assets/ssh-list.sh";
+          upgrade = "nix-rebuild && ~/Applications/Paperless/update.sh && softwareupdate --list";
+        };
+        variables = {
+          EDITOR = "vim";
+        };
+      };
+      nix.settings.experimental-features = "nix-command flakes"; # Necessary for using flakes on this system.
+      nixpkgs = {
+        config.allowUnfree = true;
+        hostPlatform = "aarch64-darwin"; # The platform the configuration will be used on.
+      };
+      system = {
+        configurationRevision = self.rev or self.dirtyRev or null; # Set Git commit hash for darwin-version.
+        primaryUser = "nikcani";
+        stateVersion = 6; # Used for backwards compatibility, please read the changelog before changing. $ darwin-rebuild changelog
+      };
+    };
+  in {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#nikbook
+    darwinConfigurations."nikbook" = nix-darwin.lib.darwinSystem {
+      modules = [
+        configuration
+        ./homebrew.nix
+        ./packages.nix
+      ];
+    };
+  };
+}
---
+++ homebrew.nix
@@ -0,0 +1,43 @@
+{...}: {
+  homebrew = {
+    casks = [
+      {name = "altair-graphql-client";}
+      {name = "atv-remote";}
+      {name = "balenaetcher";}
+      {name = "basictex";}
+      {name = "diffusionbee";}
+      {name = "discord";}
+      {name = "docker-desktop";}
+      {name = "dupeguru";}
+      {name = "gimp";}
+      {name = "handbrake-app";}
+      {name = "imageoptim";}
+      {name = "latest";}
+      {name = "libreoffice";}
+      {name = "lm-studio";}
+      {name = "mediathekview";}
+      {name = "microsoft-auto-update";}
+      {name = "microsoft-teams";}
+      {name = "monitorcontrol";}
+      {name = "mqttx";}
+      {name = "obsidian";}
+      {name = "openvpn-connect";}
+      {name = "pgadmin4";}
+      {name = "raspberry-pi-imager";}
+      {name = "scribus";}
+      {name = "stats";}
+      {name = "steam";}
+      {name = "typora";}
+      {name = "upscayl";}
+      {name = "visual-studio-code";}
+      {name = "vlc";}
+      {name = "vnc-viewer";}
+    ];
+    enable = true;
+    greedyCasks = true;
+  };
+}
+# TODO: https://nix-darwin.github.io/nix-darwin/manual/index.html#opt-homebrew.enable
+# TODO: https://nix-darwin.github.io/nix-darwin/manual/index.html#opt-homebrew.masApps
+# TODO: https://nix-darwin.github.io/nix-darwin/manual/index.html#opt-homebrew.greedyCasks
+
